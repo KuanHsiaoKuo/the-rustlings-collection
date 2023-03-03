dfb575188079a453a7840c0cf9b7119303e0e122
KuanHsiaoKuo <16441042+KuanHsiaoKuo@users.noreply.github.com>
2023-03-03 14:34:42 +0000

    Deploying to gh-pages from @ KuanHsiaoKuo/the-rustlings-collection@2ed3584025a54e902f027ae3d18e34b36c880ea5 🚀

--- error_handling/errors1.rs
+++ error_handling/errors1.rs
@@ -16,6 +16,26 @@ pub fn generate_nametag_text(name: String) -> Option<String> {
     }
 }
 
+// convert unit tests to main
+fn main() {
+    fn generates_nametag_text_for_a_nonempty_name() {
+        assert_eq!(
+            generate_nametag_text("Beyoncé".into()),
+            Ok("Hi! My name is Beyoncé".into())
+        );
+    }
+
+    fn explains_why_generating_nametag_text_fails() {
+        assert_eq!(
+            generate_nametag_text("".into()),
+            // Don't change this line
+            Err("`name` was empty; it must be nonempty.".into())
+        );
+    }
+    generates_nametag_text_for_a_nonempty_name();
+    explains_why_generating_nametag_text_fails();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
