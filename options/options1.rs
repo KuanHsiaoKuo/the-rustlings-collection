2a8ad347d1302b08154c10a7fab9ad2afc2e0bb7
KuanHsiaoKuo <16441042+KuanHsiaoKuo@users.noreply.github.com>
2023-03-03 14:39:12 +0000

    Deploying to gh-pages from @ KuanHsiaoKuo/the-rustlings-collection@e938335a90e57d9e84eb413e5f2479742f051125 🚀

--- options/options1.rs
+++ options/options1.rs
@@ -10,7 +10,26 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a value of 0
     // The Option output should gracefully handle cases where time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+    ? ? ?
+}
+
+// convert unit tests to main
+fn main() {
+    fn check_icecream() {
+        assert_eq!(maybe_icecream(9), Some(5));
+        assert_eq!(maybe_icecream(10), Some(5));
+        assert_eq!(maybe_icecream(23), Some(0));
+        assert_eq!(maybe_icecream(22), Some(0));
+        assert_eq!(maybe_icecream(25), None);
+    }
+
+    fn raw_value() {
+        // TODO: Fix this test. How do you get at the value contained in the Option?
+        let icecreams = maybe_icecream(12);
+        assert_eq!(icecreams, 5);
+    }
+    check_icecream();
+    raw_value();
 }
 
 #[cfg(test)]
